ca36a29645a9f78ac16d0c88322a9162c6be43d5
Gauravyb1325 <gauravyb1325@gmail.com>
2024-11-20 06:16:38 +0530

    last_commit

--- src/actions/api.jsx
+++ src/actions/api.jsx
@@ -1,10 +1,12 @@
-import axios from 'axios';
-
 export async function fetchApi(setTickets, setUsers) {
   try {
-    const response = await axios.get('https://api.quicksell.co/v1/internal/frontend-assignment ');
-    setTickets(response.data.tickets);
-    setUsers(response.data.users);
+    const response = await fetch('https://api.quicksell.co/v1/internal/frontend-assignment');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    setTickets(data.tickets);
+    setUsers(data.users);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
