9515d30e19a12c349b5215a9d6a323b6f5eb8a3a
Gauravyb1325 <gauravyb1325@gmail.com>
2024-11-20 06:08:30 +0530

    QuickSell_Commit

---
+++ src/assets/images.js
@@ -0,0 +1,35 @@
+import cancel from "./Cancelled.svg";
+import dashed_circle from "./Todo.svg";
+import done from "./Done.svg";
+import down from "./down.svg";
+import fade_circle from "./fade_circle.png";
+import grey_exclamation from "./UrgentPriorityGrey.svg";
+import high from "./HighPriority.svg";
+import in_progress from "./InProgress.svg";
+import low from './LowPriority.svg';
+import medium from "./MediumPriority.svg";
+import urgent from './UrgentPriorityColor.svg';
+import plus from "./add.svg";
+import no_priority from "./NoPriority.svg";
+import three_dot from './DotMenu.svg';
+import display_filter from './Display.svg';
+import void_circle from './Backlog.svg';
+
+export {
+    cancel,
+    dashed_circle,
+    done,
+    down,
+    fade_circle,
+    grey_exclamation,
+    high,
+    in_progress,
+    low,
+    medium,
+    urgent,
+    plus,
+    no_priority,
+    three_dot,
+    display_filter,
+    void_circle
+}
